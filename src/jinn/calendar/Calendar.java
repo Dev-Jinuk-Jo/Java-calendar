1973dcc21765d348f26b493684771767392107a4
Dev-Jinuk-Jo <dev.jinuk.jo@gmail.com>
2021-03-29 21:37:49 +0900

    while문을 사용하여 입력값을 계속 받는다.

--- src/jinn/calendar/Calendar.java
+++ src/jinn/calendar/Calendar.java
@@ -6,21 +6,28 @@ public class Calendar {
 	
 	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int maxDaysOfMonth(int month) {
+	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 		
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
-
-		for (int i = 0; i < repeat; i++) {
+		int month = 1;
+		
+		while (true) {
 			System.out.println("달을 입력하세요");
-			int month = scanner.nextInt();
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
 			
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.maxDaysOfMonth(month));
+			if (month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));			
 		}
 		
 		System.out.println("Bye!");
